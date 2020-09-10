4917db14bdbda2669a5e8ad91b5df766a6e332a6
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-09-10 01:35:07 -0300

    feat: add fixed content and schedule to Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Calendar,
+  Schedule,
+  Content,
+  NextAppointment,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
@@ -26,6 +35,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Hórarios agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 10</span>
+            <span>Quinta-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
+                alt="Sergio Neto"
+              />
+              <strong>Sérgio Neto</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar></Calendar>
+      </Content>
     </Container>
   );
 };
