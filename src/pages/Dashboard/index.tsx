51f94ec2f27f5503639feb49e2ad3ba2827fb848
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-09-10 23:52:19 -0300

    feat: add calendar styles and logic

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   Container,
   Header,
@@ -8,12 +8,28 @@ import {
   Schedule,
   Content,
   NextAppointment,
+  Section,
+  Appointment,
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+interface CalendarModifiers {
+  available: boolean;
+}
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
+
   const { signOut, user } = useAuth();
 
   return (
@@ -59,8 +75,85 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock></FiClock>
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
+                  alt="Sergio Neto"
+                />
+                <strong>Sérgio Neto</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock></FiClock>
+                13:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
+                  alt="Sergio Neto"
+                />
+                <strong>Sérgio Neto</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock></FiClock>
+                14:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
+                  alt="Sergio Neto"
+                />
+                <strong>Sérgio Neto</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
-        <Calendar></Calendar>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
